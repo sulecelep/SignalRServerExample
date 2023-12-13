ae362a95b5eb2a935d68863a451f5aa94e3d2c2d
ssule <ssulecelepp@gmail.com>
2023-12-13 16:54:45 +0300

    Add project files.

---
+++ SignalRServerExample/Hubs/MyHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRServerExample.Hubs
+{
+    public class MyHub :Hub
+    {
+        public async Task SendMessageAsync(string message)
+        {
+            await Clients.All.SendAsync("receiveMessage", message); //client tarafında receiveMessage adında metot bekliyor
+            
+        }
+    }
+}
---
+++ SignalRServerExample/Program.cs
@@ -0,0 +1,28 @@
+using SignalRServerExample.Hubs;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SignalRServerExample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddCors(options => options.AddDefaultPolicy(policy=>
+                policy.AllowAnyMethod()
+                       .AllowAnyHeader()
+                       .AllowCredentials()
+                       .SetIsOriginAllowed(origin => true)
+            ));
+            builder.Services.AddSignalR();
+
+            var app = builder.Build();
+
+            //app.MapGet("/", () => "Hello World!");
+            app.UseCors();
+            //https://localhost:7229/myhub 
+            app.MapHub<MyHub>("/myhub");
+            app.Run();
+        }
+    }
+}
